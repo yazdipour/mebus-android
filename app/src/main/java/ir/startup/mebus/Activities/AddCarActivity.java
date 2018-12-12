5a0132ef8ef70a3149e7a79c5c1f9423b2d95258
Shahriar Yazdipour <shahriar.yazdipour@outlook.com>
2018-12-13 00:53:07 +0330

    Max UI for AddActivity_s

--- app/src/main/java/ir/startup/mebus/Activities/AddCarActivity.java
+++ app/src/main/java/ir/startup/mebus/Activities/AddCarActivity.java
@@ -1,9 +1,11 @@
 package ir.startup.mebus.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import ir.hamsaa.RtlMaterialSpinner;
 import ir.startup.mebus.R;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class AddCarActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class AddCarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
+        try {
+            String[] ITEMS = {"ایتم اول", "ایتم دوم", "ایتم سوم", "ایتم چهارم", "ایتم پنجم", "ایتم ششم"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, ITEMS);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            RtlMaterialSpinner spinner = findViewById(R.id.spinner);
+            spinner.setAdapter(adapter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        spinner2.setAdapter(adapter);
     }
 }
--- app/src/main/java/ir/startup/mebus/Fragments/PaziraeFragment.java
+++ app/src/main/java/ir/startup/mebus/Fragments/PaziraeFragment.java
@@ -1,6 +1,7 @@
 package ir.startup.mebus.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 
 import com.crowdfire.cfalertdialog.CFAlertDialog;
 
+import ir.startup.mebus.Activities.AddPaziraeActivity;
 import ir.startup.mebus.Helpers.Utils;
 import ir.startup.mebus.R;
 
@@ -31,7 +33,7 @@ public class PaziraeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pazirae, container, false);
-
+        view.findViewById(R.id.fab).setOnClickListener(view1 -> startActivity(new Intent(getActivity(), AddPaziraeActivity.class)));
         Utils.setUpRecyclerView(view, getContext(), R.layout.item_pazirae, () -> {
             CFAlertDialog.Builder builder = new CFAlertDialog.Builder(getContext()).setDialogStyle(CFAlertDialog.CFAlertStyle.ALERT);
             ImageView img = new ImageView(getContext());
